2f43fd517197cfdb7e0478eb9abde321ebe3387b
AmitUP96 <tomarsinghjan9@gmail.com>
2024-09-04 16:19:27 +0000

    basics

--- test.js
+++ test.js
@@ -1,1 +1,13 @@
-console.log("amit");
+// console.log("amit");
+
+const name = "AMIT SINGH";
+let course = "B.Tech";
+var marks = 720;
+//  console.log(name)
+//  console.table([name, course,marks])
+  marks= 490;
+//   console.table([name, course,marks])
+
+  console.log(typeof null) // o/p will be  Object
+  console.log(typeof undefined) // o/p will be undefined
+  
