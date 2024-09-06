010f14f218024d10168978ace6e5dd54a4a0bfa6
AmitUP96 <tomarsinghjan9@gmail.com>
2024-09-06 16:59:00 +0000

    basics

--- Arrays.js
+++ Arrays.js
@@ -5,14 +5,27 @@
 // let arr2 = arr1.join()
 // console.log(arr2) // type is string
 
-let arr1 = [1,2,3,4,5,6,7]
-// console.log("A" , arr1)
+// let arr1 = [1,2,3,4,5,6,7]
+// // console.log("A" , arr1)
 
-// console.log(arr1.slice(1,4)) // it does not manipulate the original array
-// console.log(arr1)
+// // console.log(arr1.slice(1,4)) // it does not manipulate the original array
+// // console.log(arr1)
 
-let arr2 = arr1.splice(0,3)
-console.log(arr2)
-console.log( "B", arr1) // it manipulates the originals array
+// let arr2 = arr1.splice(0,3)
+// console.log(arr2)
+// console.log( "B", arr1) // it manipulates the originals array
+
+// let arr1 = [1,"amit", 2, 3]
+// let arr2 = [2,"singh", 4 ,5]
+
+// // split method  add arrays in one array
+// const main_array = [...arr1,...arr2]
+// console.log(main_array)
+
+// const mix_array = [1,2,3,[5,6,78],9,3,9]
+// const real_array = mix_array.flat(Infinity)
+// console.log(real_array)
+
+console.log(Array.from("Amit"))
 
 
