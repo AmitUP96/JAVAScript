3e6305999978f703c3cd0f30bf98762dba91928c
AmitUP96 <tomarsinghjan9@gmail.com>
2024-09-06 04:37:03 +0000

    change in

---
+++ conversion.js
@@ -0,0 +1,50 @@
+// Primitive.  7
+// String,Number,Boolean,null,undefined,Symbol,BigInt
+
+
+let num1 = Symbol("123")
+let num2 = Symbol("123")
+console.log(num1==num2)
+
+//nonPrimitive   reference type
+// Array,Objects,Functions
+
+
+
+
+
+// let num="33"
+// console.log(typeof(num))
+// let score = Number(num)
+// console.log(typeof(score))
+// console.log(score)
+
+// "33abc" => NaN
+//true => 1 , false => 0
+
+// let isAm = "Amit"
+// let isbool = Boolean(isAm)
+//  console.log(isbool)
+// console.log(typeof(isbool))
+
+//1 => true , 0 => false
+// "" =>false
+//"Amit" => true
+
+//Operations
+
+// let sum = 3+3
+// console.log(sum)
+// let value = -sum
+// console.log(value)
+
+//Compasion
+
+// console.log("2" > 1)   //"2" automatically converted into Number 
+
+// console.log(null > 0)
+// console.log(null == 0)
+// console.log(null < 0)
+// console.log(null >= 0) // true  js automatically convert null into Number
+
+
